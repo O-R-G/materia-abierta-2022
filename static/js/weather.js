20b184baca15827abf566cf59f5fb75c24b1f5d3
wei <wei@o-r-g.com>
2022-04-12 08:23:19 +0800

    fallbacks

--- static/js/weather.js
+++ static/js/weather.js
@@ -3,21 +3,48 @@ var body = document.body;
 var bodyClass = '';
 var r = document.querySelector(':root');
 var request_weather = new XMLHttpRequest();
+var request_static_weather = new XMLHttpRequest();
+var string_weather = '';
+request_static_weather.onreadystatechange = function(){
+	if (request_static_weather.readyState === 4) {
+		if (request_static_weather.status === 200) { 
+			try{
+                if(request_static_weather.responseText)
+                	string_weather = request_static_weather.responseText;
+                if(typeof typewriter === 'function')
+					typewriter(string_weather, sWeather, 200, nextStage);
+				else
+				{
+					window.addEventListener('load', function(){
+						if(typeof typewriter === 'function')
+							typewriter(string_weather, sWeather, 200, nextStage);
+						else
+							console.log('no typewriter');
+					});
+				}
+			}
+			catch(err)
+			{
+				typewriter(string_weather, sWeather, 200, nextStage);
+			}
+		}
+	}
+};
+request_static_weather.open('GET', 'static/txt/weather.txt');
+
 request_weather.onreadystatechange = function(){
 	if (request_weather.readyState === 4) {
 		if (request_weather.status === 200) { 
 			try{
                 var data= JSON.parse(request_weather.responseText)['current'];
-				console.log(data);
-				console.log(data['is_day']);
 				var isDay = data['is_day'] != 0;
-				console.log('isDay = '+isDay);
 				bodyClass += isDay ? 'is_day' : ' is_night';
 				body.classList = bodyClass;
 				// console.log(r);
-				if(typeof data['temp_f'] != 'undefined')
+				// console.log(data);
+				if(typeof data['temp_c'] != 'undefined')
 				{
-					let temp = data['temp_f'];
+					let temp = data['temp_c'];
 					let temp_max = 76;
 					let temp_min = 38;
 					let hue =  (parseInt((temp - temp_min) / (temp_max - temp_min) * 360) + 180) % 360;
@@ -27,11 +54,11 @@ request_weather.onreadystatechange = function(){
 					}
 					else
 					{
-						
 						var s = '100%';
 						var l = '10%';
 					}
 					r.style.setProperty('--background-color', 'hsl('+hue+', '+s+', '+l+')');
+					string_weather = 'Currently ' + temp + '° C';
 				}
 				if(typeof data['humidity'] != 'undefined')
 				{
@@ -45,7 +72,6 @@ request_weather.onreadystatechange = function(){
 					}
 					else
 					{
-						
 						var s = '100%';
 						var l = '80%';
 					}
@@ -68,15 +94,32 @@ request_weather.onreadystatechange = function(){
 					}
 					r.style.setProperty('--highlight-color', 'hsl('+hue+', '+s+', '+l+')');
 				}
+				if(typeof data['condition'] != 'undefined' && typeof data['condition']['text'] != 'undefined')
+				{
+					if(string_weather != '')
+						string_weather += '. ';
+					string_weather += data['condition']['text'];
+				}
+
+				if(typeof typewriter === 'function')
+					typewriter(string_weather, sWeather, 200, nextStage);
+				else
+				{
+					window.addEventListener('load', function(){
+						if(typeof typewriter === 'function')
+							typewriter(string_weather, sWeather, 200, nextStage);
+
+					});
+				}
 			}
 			catch(err)
-			{
-				console.log('Error!');
-				console.log(err);
-				return false;
-			}
+				request_static_weather.send();
+		}
+		else
+		{
+			// console.log('not 200');
+			request_static_weather.send();
 		}
 	}
 };
-request_weather.open('GET', location.protocol + '//api.weatherapi.com/v1/current.json?key=5262904081d248dc9d6134509221701&q=Mexico City');
-request_weather.send();
+request_weather.open('GET', location.protocol + '//api.weatherapi.com/v1/current.json?key=5262904081d248dc9d6134509221701&q=Milpa Alta');
