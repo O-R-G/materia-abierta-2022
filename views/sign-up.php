175d5b985077d50eb7cdc2dc53146fc6c46a1db4
wei <wei@o-r-g.com>
2022-04-29 09:15:42 +0800

    add sign-up .php

---
+++ views/sign-up.php
@@ -0,0 +1,53 @@
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<!-- <link rel="stylesheet" href="/static/css/fonts.css"> -->
+	<link rel="stylesheet" href="/static/css/main.css">
+	<style>
+		body{
+			background-color: transparent;
+		}
+		input[type=submit] {
+			-webkit-appearance:  none;
+			-moz-appearance: none;
+			appearance: none;
+			background-color: #333;
+			color: #FFF;
+  			padding-left: 10px;
+  			padding-right: 10px;
+		  	border: none;
+  			border-radius: 4px;
+  			cursor: pointer;
+  			font-size: 16px;
+		}
+		.clear {
+    			clear: none;
+		}
+		.mc-field-group input {
+			display: inline-block;
+		}
+	</style>
+</head>
+<body style="overflow: hidden;">
+<!-- Begin Mailchimp Signup Form -->
+<div id="mc_embed_signup">
+<form action="https://materiaabierta.us20.list-manage.com/subscribe/post?u=4ba46b0d536050a2aa447cd72&amp;id=1a9ea3f449" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+    <div id="mc_embed_signup_scroll">
+
+<div class="mc-field-group" style="margin-bottom: 4px;">
+	<!-- <label for="mce-EMAIL">Email Address </label> -->
+	<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
+</div>
+	<div id="mce-responses" class="clear">
+		<div class="response" id="mce-error-response" style="display:none"></div>
+		<div class="response" id="mce-success-response" style="display:none"></div>
+	</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_4ba46b0d536050a2aa447cd72_1a9ea3f449" tabindex="-1" value=""></div>
+    <div class="clear"><input type="submit" value="<?= isset($_GET['en']) ? 'Subscribe' : 'Suscribirse'; ?>" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+    </div>
+</form>
+</div>
+
+<!--End mc_embed_signup-->
+
+</body>
